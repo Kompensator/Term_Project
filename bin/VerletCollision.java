29196aded0e739a306ebc8cac4e2fb320605c96d
alexda4 <alexda4@yahoo.ca>
2019-04-30 12:37:30 -0400

    i did it again

--- bin/VerletCollision.java
+++ bin/VerletCollision.java
@@ -9,7 +9,7 @@ public class VerletCollision {
 
     public static void main (String[] args) throws IOException {
         // File file = new File("/Users/DY/java/Term Project/position2.data");
-		File file = new File("c://Users//alexd//Documents//School//Winter2019//CompSci//TermProject//Term_Project//testdata.data");
+		File file = new File("c://Users//alexd//Documents//School//Winter2019//CompSci//TermProject//Term_Project//bin//testdata.data");
         // File file = new File("your path here");
 		
         FileWriter writer = new FileWriter(file);
