26e150cb4023cb3eba6bad1f7062e5e5a506a8d6
DY <23586296+Kompensator@users.noreply.github.com>
2019-05-11 16:22:54 -0400

    created animate_SEM.py for SEM animation animation now looks better

--- bin/SunEarthMoon.java
+++ bin/SunEarthMoon.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.*;
 
-public class Verlet {
+public class SunEarthMoon {
 	/**
 	Now uses the same Body class as VerletCollision
 	The Body class in this file is commented out
@@ -17,17 +17,16 @@ public class Verlet {
         int n = 3;      // number of bodies
         double simTime = 1e9;
         int totalSteps = (int) Math.round(simTime / dt);
-		double spread = 1.4e11;		// determines the square where the bodies are generated
+		double spread = 1.5e11;		// determines the square where the bodies are generated
         
-        //File file = new File("/Users/DY/Term_Project/bin/testdata.data");       // data file
-        File file = new File("c://Users//alexd//Documents//School//Winter2019//CompSci//TermProject//Term_Project//bin//testdata.data"); //davids laptop
+        File file = new File("/Users/DY/Term_Project/bin/testdata.data");       // data file
+        // File file = new File("c://Users//alexd//Documents//School//Winter2019//CompSci//TermProject//Term_Project//bin//testdata.data"); //davids laptop
 		
 		// File file = new File("your path here")
         FileWriter writer = new FileWriter(file);
 
-		//File config = new File("/Users/DY/Term_Project/bin/sim.cfg");           // config file for animation
-		File config = new File("c://Users//alexd//Documents//School//Winter2019//CompSci//TermProject//Term_Project//bin//testdata.data"); //davids laptop
-		// File config = new File("your path here");
+		File config = new File("/Users/DY/Term_Project/bin/sim.cfg");           // config file for animation
+		// File config = new File("c://Users//alexd//Documents//School//Winter2019//CompSci//TermProject//Term_Project//bin//testdata.data"); //davids laptop
         FileWriter cfgWriter = new FileWriter(config);
         
 		// writing config to config file
@@ -44,7 +43,7 @@ public class Verlet {
         //     bodies[i] = new Body(randint(-1.4e12, 1.4e12), randint(-1.4e12, 1.4e12), randint(-2e5, 2e5), randint(-2e5, 2e5), randint(7.5e22, 3e30), 1, bodyName);
         // }
         bodies[0] = new Body(1.496e11, 0, 0, 29741, 5.97e24, 1e4, "Earth");
-       bodies[1] = new Body(0, 0, 0, 0, 1.989e30, 1e7, "Sun");
+        bodies[1] = new Body(0, 0, 0, 0, 1.989e30, 1e7, "Sun");
 		bodies[2] = new Body(1.49984e11, 0, 0, 30764, 7.348e22, 1e4, "Moon");
 
         for (int step = 0; step < totalSteps; step++) {
---
+++ bin/animate_SEM.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+
+
+# settings
+global n, number_of_dots
+
+data_file = "testdata.data"
+cfg_file = "sim.cfg"
+cfg = open(cfg_file, "r")
+
+# read settings fro sim.cfg
+n = int(cfg.readline())
+total_steps = int(cfg.readline())
+number_of_dots = 150
+# spread determines the bound of the animation
+spread = 2.4e11
+file = open(data_file, "r")
+
+# trying to write the animation to a file
+# Writer = animation.writers['html']
+# writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+
+# setting up fig and axis
+fig, ax = plt.subplots(figsize=(9,12))
+plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+ax.set_facecolor('xkcd:black')
+ax.set_xlim(-spread, spread)
+ax.set_ylim(-spread, spread)
+
+
+class body():
+    def __init__(self):
+        self.x = []
+        self.y = []
+        self.mass = 0.0
+
+def update(frame):
+    trail_x, trail_y = [], []
+
+    for i in range(n):
+        x = bodies[i].x[frame]
+        y = bodies[i].y[frame]
+
+        # merge handling
+        if not (x == 0 and y == 0):
+            animated_bodies[i].set_data(x,y)
+        else:
+            animated_bodies[i].set_data([],[])
+
+    # writing in the trace
+    for i in range(n):
+        body = bodies[i]
+        if frame < number_of_dots:
+            trail_x.append(body.x[0:frame])
+            trail_y.append(body.y[0:frame])
+
+        else:
+            trail_x.append(body.x[(frame-number_of_dots):frame])
+            trail_y.append(body.y[(frame-number_of_dots):frame])
+
+    animated_bodies[n].set_data(trail_x,trail_y)
+
+    return animated_bodies
+
+
+# creating objects to that contains the position data
+bodies = []
+
+for i in range(n):
+    bodies.append(body())
+
+for i in range(total_steps):
+    for j in range(n):
+        try:
+            data = [float (n) for n in file.readline().split()];
+            bodies[j].x.append(data[1])
+            bodies[j].y.append(data[2])
+            bodies[j].mass = data[3]
+        except:
+            break
+
+# creating aniated objects
+for i in range(n):
+    earth_plot, = plt.plot([],[],color='#0077be',marker='o',markersize=5,animated=True)
+    sun_plot, = plt.plot([],[],color='#ffff00',marker='o',markersize=12,animated=True)
+    moon_plt, = plt.plot([],[],color='#f5f3ce',marker='o',markersize=3,animated=True)
+
+    animated_bodies = [earth_plot, sun_plot, moon_plt]          # the packing order is important
+
+# creating trail of bodies
+trail, = plt.plot([], [], 'bo', markersize=0.2, animated=True)
+animated_bodies.append(trail)
+ani = FuncAnimation(fig, update, interval=1, blit=True)
+plt.show()
+
+
+# fig1 = plt.figure()
+# im_ani = animation.ArtistAnimation(fig1, ims, interval=50, repeat_delay=3000,
+#                                    blit=True)
+# im_ani.save('im.mp4', writer=writer)
