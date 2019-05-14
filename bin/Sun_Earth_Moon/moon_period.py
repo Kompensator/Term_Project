2c8b97f3d30ee13a0d152a562bb7e643b7aa0708
DY <23586296+Kompensator@users.noreply.github.com>
2019-05-14 19:53:19 -0400

    Optimized moon_period.py

--- bin/Sun_Earth_Moon/moon_period.py
+++ bin/Sun_Earth_Moon/moon_period.py
@@ -1,13 +1,4 @@
 import numpy as np
-from sys import exit
-
-sim_time = 3.154e7
-dt = 600
-n = 3
-total_steps = int(sim_time//dt)
-
-file = open("testdata.data",'r')
-
 
 class body():
     def __init__(self):
@@ -16,47 +7,55 @@ class body():
     
 
 def unit_vector(vector):
-    return vector/np.linalg.norm(vector)
+    norm = (vector[0]**2 + vector[1]**2)**0.5
+    return [dim/norm for dim in vector]
 
 
 def angle_between(v1, v2):
     u1 = unit_vector(v1)
     u2 = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(u1, u2), -1.0, 1.0))
+    return np.arccos(np.dot(u1, u2))
 
+def main():
+    sim_time = 3.154e7
+    dt = 600
+    n = 3
+    total_steps = int(sim_time//dt)
 
-bodies = []
+    file = open("testdata.data",'r')
+    bodies = []
 
-for i in range(n):
-    bodies.append(body())
+    for i in range(n):
+        bodies.append(body())
 
-for i in range(total_steps):
-    for j in range(n):
-        try:
-            data = [float (n) for n in file.readline().split()];
-            bodies[j].x.append(data[1])
-            bodies[j].y.append(data[2])
-        except:
-            break
+    for i in range(total_steps):
+        for j in range(n):
+            try:
+                data = [float (n) for n in file.readline().split()];
+                bodies[j].x.append(data[1])
+                bodies[j].y.append(data[2])
+            except:
+                break
 
-orbit_count = 0
-last_earth_angle = 0
-last_moon_angle = 0
-for i in range(total_steps):
-    """ processing loop
-    """
-    v1 = [bodies[0].x[i] - bodies[1].x[i], bodies[0].y[i] - bodies[1].y[i]]         # earth vector
-    v2 = [bodies[2].x[i] - bodies[1].x[i], bodies[2].y[i] - bodies[1].y[i]]         # sun vector
+    orbit_count = 0
+    last_earth_angle = 0
+    last_moon_angle = 0
+    for i in range(total_steps):
+        """ processing loop
+        """
+        v1 = [bodies[0].x[i] - bodies[1].x[i], bodies[0].y[i] - bodies[1].y[i]]         # earth vector
+        v2 = [bodies[2].x[i] - bodies[1].x[i], bodies[2].y[i] - bodies[1].y[i]]         # sun vector
 
-    earth_angle = angle_between(v1, [1, 0])
-    moon_angle = angle_between(v2, [1, 0])
+        earth_angle = angle_between(v1, [1, 0])
+        moon_angle = angle_between(v2, [1, 0])
 
-    if earth_angle < moon_angle and last_earth_angle > last_moon_angle:
-        # orbit_count += 1
-        print(i)
-        exit(0)
+        if earth_angle < moon_angle and last_earth_angle > last_moon_angle:
+            orbit_count += 1
 
-    last_earth_angle = earth_angle
-    last_moon_angle = moon_angle
+        last_earth_angle = earth_angle
+        last_moon_angle = moon_angle
 
-print(orbit_count)
+    print(orbit_count)
+
+if __name__ == "__main__":
+    main()
